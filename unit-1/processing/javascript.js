b3b119abb9def18b6f25cc59d7c3e52a1009c4be
DarkMatr126 <95891701+DarkMatr126@users.noreply.github.com>
2022-01-05 08:42:06 -0700

    Update javascript.js

--- unit-1/processing/javascript.js
+++ unit-1/processing/javascript.js
@@ -280,10 +280,20 @@ function roll() {
 }
 
 function quesSlt(a) {
-    let ques = Math.ciel(Math.random()*quesNum);
-    setTimeout(function() {document.getElementById(`ques${ques}`).style.display = "block"},500);
-    if (quesDone.indexOf(ques) > -1) {
-        quesDone.splice(quesDone.indexOf(ques),1);
+    alet(quesDon.length);
+    if (quesDone.length !=0) {
+        let ques = Math.ceil(Math.random()*(quesDone.length)) - 1;
+        setTimeout(function() {document.getElementById(`ques${quesDone[ques]}`).style.display = "block"},500);
+        
+        if (quesDone.indexOf(ques) > -1) {
+            quesDone.splice(quesDone.indexOf(ques),1);
+        }
+    } else {
+        let ques = Math.ciel(Math.random()*quesNum);
+        setTimeout(function() {document.getElementById(`ques${ques}`).style.display = "block"},500);
+        if (quesDone.indexOf(ques) > -1) {
+            quesDone.splice(quesDone.indexOf(ques),1);
+        }
     }
     if (a === 1) {
         quesExtra = 1;
